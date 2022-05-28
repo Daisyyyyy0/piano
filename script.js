2f7ec7a0fdaf4ffba9db350efad94b2fcdfe82c5
Daisy <soniahsiao16@gmail.com>
2022-05-28 23:48:00 +0800

    problems of keydown

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 const keys = document.querySelectorAll('.key')
 
+const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j']
+const whiteKeys = document.querySelectorAll('.key.white')
+const blackKeys = document.querySelectorAll('.key.black')
+
 keys.forEach( key => {
     key.addEventListener('click', () => playNote(key))
 })
+
+document.addEventListener('keydown', e => {
+    if (e.repeat) return
+    const key = e.key
+    const whiteKeyIndex = WHITE_KEYS.indexOf(key)
+    const blackKeyIndex = BLACK_KEYS.indexOf(key)
+
+    console.log(whiteKeyIndex);
+    console.log(blackKeyIndex);
+    if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex])  //這兩行無法運作，key無法和noteAudio元素連接
+    if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex])
+  })
 
 function playNote(key){
     const noteAudio = document.getElementById(key.dataset.note)
